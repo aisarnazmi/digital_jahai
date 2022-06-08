cd65c323a8baf9d9fc1391e1ab9c2b5fbaf4b966
Aisar Nazmi <aisar.nazmiey39@gmail.com>
2022-06-08 23:31:20 +0800

    upgrade jdk version to fix build runtime error

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/aisarnazmi/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aisarnazmi/Developer/digital_jahai"
+export "FLUTTER_ROOT=/Users/luminous/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/luminous/Developer/digital_jahai"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
